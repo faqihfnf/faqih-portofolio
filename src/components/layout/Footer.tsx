47c1050c58b7e61490a5c8cfd4a06483dd1f2d73
faqihfnf <faqih.fnf@gmail.com>
2025-07-22 11:30:16 +0700

    initial commit and adding Navbar, footer and implementation dark mode

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,52 @@
+import { Github, Linkedin, Twitter, Youtube } from "lucide-react";
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer className="p-6 bg-[rgb(5,10,35)]">
+      <div className="max-w-7xl mx-auto">
+        <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4">
+          {/* Kiri: Nama */}
+          <p className="text-md text-gray-300 text-center sm:text-left">
+            Created with ❤️{" "}
+            <span className="bg-gradient-to-l from-emerald-400 via-teal-400 to-cyan-400 bg-clip-text text-md font-extrabold text-transparent">
+              Faqih Nur Fahmi
+            </span>
+          </p>
+
+          {/* Kanan: Icon Sosial Media */}
+          <div className="flex justify-center sm:justify-end gap-4">
+            <Link
+              href="https://github.com/username"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 rounded-lg bg-black-200 border border-indigo-400 dark:border-slate-700 hover:bg-indigo-600 hover:text-white text-white hover:border-indigo-600">
+              <Github className="w-5 h-5" />
+            </Link>
+            <Link
+              href="https://linkedin.com/in/username"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 rounded-lg bg-black-200 border border-indigo-400 dark:border-slate-700 hover:bg-indigo-600 hover:text-white text-white hover:border-indigo-600">
+              <Linkedin className="w-5 h-5" />
+            </Link>
+            <Link
+              href="https://twitter.com/username"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 rounded-lg bg-black-200 border border-indigo-400 dark:border-slate-700 hover:bg-indigo-600 hover:text-white text-white hover:border-indigo-600">
+              <Twitter className="w-5 h-5" />
+            </Link>
+            <Link
+              href="https://twitter.com/username"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 rounded-lg bg-black-200 border border-indigo-400 dark:border-slate-700 hover:bg-indigo-600 hover:text-white text-white hover:border-indigo-600">
+              <Youtube className="w-5 h-5" />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import React from "react";
+import {
+  Home,
+  User,
+  Briefcase,
+  BookOpen,
+  Mail,
+  ShieldAlertIcon,
+  ShieldCheckIcon,
+  BriefcaseBusinessIcon,
+} from "lucide-react";
+import { FloatingNav } from "../ui/floating-navbar";
+
+export default function Navbar() {
+  const navItems = [
+    {
+      name: "Home",
+      link: "/",
+      icon: <Home className="h-4 w-4 " />,
+    },
+    {
+      name: "Experience",
+      link: "/experience",
+      icon: <User className="h-4 w-4 " />,
+    },
+    {
+      name: "Projects",
+      link: "/projects",
+      icon: <BriefcaseBusinessIcon className="h-4 w-4 " />,
+    },
+    {
+      name: "Certificates",
+      link: "/certificates",
+      icon: <ShieldCheckIcon className="h-4 w-4 " />,
+    },
+    {
+      name: "Blog",
+      link: "/blog",
+      icon: <BookOpen className="h-4 w-4 " />,
+    },
+    {
+      name: "Contact",
+      link: "/contact",
+      icon: <Mail className="h-4 w-4 " />,
+    },
+  ];
+
+  return (
+    <div className="w-full">
+      <FloatingNav navItems={navItems} />
+    </div>
+  );
+}
